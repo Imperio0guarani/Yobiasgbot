22f821381ff8f40793786182b43da074fe121a35
Imperio0guarani <ctobiasabel@gmail.com>
2025-03-11 10:50:24 -0300

    ahora si no se cae, restructuracion del codigo

--- bot.py
+++ bot.py
@@ -3,15 +3,19 @@ from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext, Application
 import asyncio
 
-# Reemplaza 'TU_TOKEN' con el token que te proporcionó BotFather
+# Obtén el token de la variable de entorno
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 if TOKEN is None:
     raise ValueError("El token de Telegram no está configurado como variable de entorno.")
 
-
+# Definir la función start
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text('A su servicio.')
+
+# Definir la función creator
+async def creator(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text('Este bot fue creado por Tobiasg.')
 
 def main():
     # Crea la aplicación y pasa el token de tu bot.
@@ -25,10 +29,6 @@ def main():
 
     # Inicia el bot
     application.run_polling()
-
-    
-
-
 
 if __name__ == '__main__':
     main()
